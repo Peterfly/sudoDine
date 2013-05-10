51ce0df8c2df9bcb38be256b978900d37fe54335
Peter Zhenghao Qian <peterfly@berkeley.edu>
2013-05-10 14:21:21 -0700

    reorder the directory

---
+++ extract.sh
@@ -0,0 +1,3 @@
+grep 'type": "review' yelp_data.json | head -n 2000 > temp.json
+grep 'type": "business' yelp_data.json | head -n 2000 >> temp.json
+grep 'type": "user' yelp_data.json | head -n 2000 >> temp.json
