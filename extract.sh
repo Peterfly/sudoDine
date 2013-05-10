5c2acce2c714d96137e0295194a20b6973a3ec38
Peter Zhenghao Qian <peterfly@berkeley.edu>
2013-05-10 14:23:49 -0700

    clean up the repo

--- extract.sh
+++
@@ -1,3 +0,0 @@
-grep 'type": "review' yelp_data.json | head -n 2000 > temp.json
-grep 'type": "business' yelp_data.json | head -n 2000 >> temp.json
-grep 'type": "user' yelp_data.json | head -n 2000 >> temp.json
